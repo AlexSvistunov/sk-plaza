d55720c4582f2a7822553bf629f75011a2d24de4
Alexey Svistunov <xdayx53@gmail.com>
2023-11-06 23:34:12 +0300

    z-index fixes

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -3,6 +3,7 @@ const burger = document.querySelector('.burger');
 const pageBody = document.querySelector('.page__body');
 // const footerBurger = document.querySelector('.footer__burger');
 const footer = document.querySelector('.footer');
+const swipers = document.querySelectorAll('.swiper')
 
 const links = document.querySelectorAll('a[data-scroll]');
 links.forEach(link => {
@@ -10,11 +11,15 @@ links.forEach(link => {
     headerMenu.classList.remove('menu--active');
     burger.classList.remove('burger--active');
     pageBody.classList.remove('dis-scroll');
+
   });
 });
 
 burger.addEventListener('click', () => {
   footer.classList.toggle('footer-z');
+  swipers.forEach(el => {
+    el.classList.toggle('swiper-z')
+  });
 });
 
 
