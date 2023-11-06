f3d788e9ea397363b8972363956da033dbdd30ca
Alexey Svistunov <xdayx53@gmail.com>
2023-11-06 17:23:46 +0300

    for clients version

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -1,7 +1,6 @@
 const headerMenu = document.querySelector('.header__menu');
 const burger = document.querySelectorAll('.burger');
 const pageBody = document.querySelector('.page__body');
-// const footerBurger = document.querySelector('.footer__burger');
 burger.forEach(burger => {
   burger.addEventListener('click', function() {
     headerMenu.classList.toggle('active');
