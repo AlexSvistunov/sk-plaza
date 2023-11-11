bb214606e6929d40ec825a6bf6e9e7ba66a7b84b
Alexey Svistunov <xdayx53@gmail.com>
2023-11-11 15:16:39 +0300

    added modal with js

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,1 +1,12 @@
 const modal = document.querySelector('.modal');
+const contactBtn = document.querySelector('.contact__info-btn');
+const modalClose = document.querySelector('.modal__btn');
+contactBtn.addEventListener('click', function() {
+  modal.classList.add('active');
+});
+
+
+modalClose.addEventListener('click', function() {
+  modal.classList.remove('active');
+});
+
