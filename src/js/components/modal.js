f07b663c0c1c42c590a23457eb875ae11fa8a354
Alexey Svistunov <xdayx53@gmail.com>
2023-11-22 09:21:18 +0300

    fixed modal error

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,12 +1,12 @@
-const modal = document.querySelector('.modal');
-const contactBtn = document.querySelector('.contact__info-btn');
-const modalClose = document.querySelector('.modal__btn');
-contactBtn.addEventListener('click', function() {
-  modal.classList.add('active');
-});
+// const modal = document.querySelector('.modal');
+// const contactBtn = document.querySelector('.contact__info-btn');
+// const modalClose = document.querySelector('.modal__btn');
+// contactBtn.addEventListener('click', function() {
+//   modal.classList.add('active');
+// });
 
 
-modalClose.addEventListener('click', function() {
-  modal.classList.remove('active');
-});
+// modalClose.addEventListener('click', function() {
+//   modal.classList.remove('active');
+// });
 
