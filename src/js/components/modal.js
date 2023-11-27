cd6a157632cb1a58ced5870d1bde87d982982517
Alexey Svistunov <xdayx53@gmail.com>
2023-11-27 15:00:02 +0300

    added scroll behav + fixed

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -3,23 +3,28 @@ const modalCall = document?.querySelector('.modal--call');
 const signUp = document.querySelectorAll('#signUp');
 const modalReview = document.querySelector('.modal--review');
 const modalFillForm = document.querySelector('.modal--fillform');
+import { disableScroll } from "../functions/disable-scroll";
+import { enableScroll } from "../functions/enable-scroll";
 import { handleModalState } from "./modal-scroll";
 
 getConsult.forEach(el => {
   el.addEventListener('click', function(){
     modalCall.classList.add('active');
+    disableScroll()
 
   });
 })
 
 modalCall?.querySelector('.modal__btn').addEventListener('click', function(){
   modalCall.classList.remove('active');
+  enableScroll()
 
 });
 
 signUp.forEach(el => {
   el.addEventListener('click', function(){
     modalReview.classList.add('active');
+    disableScroll()
 
 
 
@@ -28,6 +33,7 @@ signUp.forEach(el => {
 
 modalReview?.querySelector('.modal__btn').addEventListener('click', function(){
   modalReview.classList.remove('active');
+  enableScroll()
 
 
 
@@ -41,10 +47,12 @@ modalReview?.querySelector('.modal-form__btn').addEventListener('click', functio
 
 
 
+
 });
 
 modalFillForm?.querySelector('.modal__btn').addEventListener('click', function(){
   modalFillForm.classList.remove('active');
+  enableScroll()
 
 
 });
